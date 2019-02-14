09bcb85f1825919f91668dc5f2723e6ca31d55f2
raanan-g <41341275+raanan-g@users.noreply.github.com>
2019-02-13 21:58:22 -0500

    Create mapbox.js

---
+++ mapbox.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFhbmFuLWciLCJhIjoiY2pyMWF5YzM4MDBseTQzcXEyZ3gxN2xvOSJ9.Jm_gHZ3zcJh2xygKeOdr5w';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/raanan-g/cjs3vdafw04bb1fmtnqdivns0',
+center: [-79.990100, 40.441700],
+zoom: 10.0
+});
